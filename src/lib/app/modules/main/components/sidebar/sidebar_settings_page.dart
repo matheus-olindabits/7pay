edc0ccf512c1bec071d4250b9593db6fd23160fe
Matheus Leal <matheus.leal@nutrin.com.br>
2024-06-06 20:47:12 -0300

    feat: alteração na foto do perfil do sidebar settigns

--- src/lib/app/modules/main/components/sidebar/sidebar_settings_page.dart
+++ src/lib/app/modules/main/components/sidebar/sidebar_settings_page.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:test_7pay/app/core/style/color.dart';
 import 'package:test_7pay/app/core/style/size.dart';
 import 'package:test_7pay/app/core/style/text.dart';
-import 'package:test_7pay/app/modules/main/controllers/main_controller.dart';
 import 'package:test_7pay/app/shared/widgets/spacing/divider_h.dart';
 
 class SidebarSettingsPage extends StatelessWidget {
-  SidebarSettingsPage({super.key});
-
-  final _controller = Modular.get<MainController>();
+  const SidebarSettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,35 +14,42 @@ class SidebarSettingsPage extends StatelessWidget {
       color: Colors.white,
       width: (context.isMobile) ? context.w(.8) : context.w(.15),
       child: ListView(
-        padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
-            decoration: const BoxDecoration(color: WColor.primaryColor),
-            child: Observer(builder: (context) {
-              return Column(
+          Container(
+            color: WColor.primaryColor,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset(
-                    'assets/images/perfil.jpg',
-                    scale: 2,
+                  ClipOval(
+                    child: SizedBox.fromSize(
+                      size: const Size.fromRadius(40),
+                      child: Image.asset(
+                        'assets/images/perfil.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                   const SpacingH(.02),
                   Text(
                     'Matheus Leal',
                     style: const TextStyle().textBold(14, Colors.white),
                   ),
+                  Text(
+                    'Administrador',
+                    style: const TextStyle().textRegular(12, Colors.white),
+                  ),
                 ],
-              );
-            }),
+              ),
+            ),
           ),
           ListTile(
             title: Text(
               'Redefinir Senha',
               style: const TextStyle().textBold(14),
             ),
-            onTap: () {
-              Modular.to.pop();
-            },
+            onTap: () => Modular.to.pop(),
           ),
           ListTile(
             leading: const Icon(
@@ -57,9 +60,7 @@ class SidebarSettingsPage extends StatelessWidget {
               'Sair',
               style: const TextStyle().textBold(14),
             ),
-            onTap: () {
-              //_controller.logout();
-            },
+            onTap: () => Modular.to.pop(),
           ),
         ],
       ),
