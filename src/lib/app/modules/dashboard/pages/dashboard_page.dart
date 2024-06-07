74a8fec27c4db5ddd5c3a9f2451913dbe9f0bdeb
Matheus Leal <matheus.leal@nutrin.com.br>
2024-06-07 13:49:29 -0300

    feat: criação do conteudo da dashboard

--- src/lib/app/modules/dashboard/pages/dashboard_page.dart
+++ src/lib/app/modules/dashboard/pages/dashboard_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:test_7pay/app/core/style/size.dart';
+import 'package:test_7pay/app/shared/enum/page_name_enum.dart';
+import 'package:test_7pay/app/shared/widgets/header/header_default_page.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
@@ -6,10 +9,33 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            HeaderDefaultPage(
+              name: PageName.dashboard.name,
+              icon: Icons.dashboard,
+            ),
+            Container(
+              height: context.h(.3),
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'assets/images/banner.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
