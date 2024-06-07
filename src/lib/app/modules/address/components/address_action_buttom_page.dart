060b0c5a1be55c74d7133596535d017e3897d991
Matheus Leal <matheus.leal@nutrin.com.br>
2024-06-07 00:03:50 -0300

    feat: criação da lista dos endereços no controller

---
+++ src/lib/app/modules/address/components/address_action_buttom_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddressActionButtomPage extends StatelessWidget {
+  const AddressActionButtomPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
